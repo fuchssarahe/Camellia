4238f8351e2911203c35efdd53faf189b420ea23
Sarah Fuchs <fuchssarahe@gmail.com>
2016-06-28 14:59:40 -0700

    added a couple quick buttons so you can now navigate to the sign up and login components

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,10 +1,21 @@
 const React = require('react');
 
 const App = React.createClass({
+  _navToSignup: function () {
+    window.location.hash = '/signup';
+  },
+
+  _navToLogin: function () {
+    window.location.hash = '/login';
+  },
+
   render: function () {
+
     return (
       <div>
         <h1>Hellow from app</h1>
+        <button onClick={this._navToSignup}>Sign Up</button>
+        <button onClick={this._navToLogin}>Login</button>
         {this.props.children}
       </div>)
   }
