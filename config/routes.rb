f85a4fb8921c236cf80c5edbe6515e34a0c13641
Sarah Fuchs <sarah@Sarahs-MacBook-Air.local>
2016-06-28 12:40:39 -0700

    back end auth compelte

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'static_pages#root'
 
-  # namespace :api default {'json'} do
-  #
-  # end
+  namespace :api, default: { format: 'json' } do
+    resources :users, only: :create
+    resource :session, only: [:create, :destroy, :show]
+  end
 end
