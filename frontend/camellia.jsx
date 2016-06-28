3376edd01c3fbbbcc12bc4c6cb12b3dc21adbd64
Sarah Fuchs <sarah@Sarahs-MacBook-Air.local>
2016-06-27 20:42:40 -0700

    project set up; root app route created

---
+++ frontend/camellia.jsx
@@ -0,0 +1,16 @@
+import { Router, Route, IndexRoute, hashHistory, Link } from 'react-router';
+const React = require('react'),
+      ReactDOM = require('react-dom'),
+      App = require('./components/app');
+
+
+const router =
+  <Router path='/' component={App}>
+  </Router>;
+
+$(
+  () => { ReactDOM.render(
+    <div>Hello from react-dom</div>,
+    document.getElementById('root')
+  )};
+);
