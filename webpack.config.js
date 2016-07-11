b298dcb2f400638b175e78d1a5c7baf5829b0a73
Sarah Fuchs <fuchssarahe@gmail.com>
2016-07-10 22:00:27 -0700

    getting new relic set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require("path");
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
   context: __dirname,
@@ -7,6 +8,15 @@ module.exports = {
     path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
   },
+  plugins: [
+    new BrowserSyncPlugin({
+      // browse to http://localhost:3000/ during development,
+      // ./public directory is being served
+      host: 'localhost',
+      port: 3000,
+      server: { baseDir: ['public'] }
+    })
+  ],
   resolve: {
     extensions: ["", ".js", ".jsx"]
   },
